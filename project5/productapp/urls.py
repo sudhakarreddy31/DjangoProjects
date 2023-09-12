2ddc8ea0c1ebbd8a514bfc8f35da8d264df34fd5
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-09-12 15:14:03 +0530

    addedCRUDoperation

--- project5/productapp/urls.py
+++ project5/productapp/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from productapp import views
 
 urlpatterns = [
-    path("product_list",views.product_list),
+    path("product_list",views.product_list,name='product_list'),
     path("product_detail/<int:id>",views.product_detail,name='product_detail'),
+    path("product_create/",views.product_create,name='product_create'),
+    path('product_update/<int:id>/',views.product_update,name='product_update'),
+    # path('product_delete/<int:id>/',views.product_delete,name='product_delete'),
+    path('product_delete/<int:id>/', views.product_delete, name='product_delete')
 
 ]
--- project5/productapp/views.py
+++ project5/productapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-
+from productapp.forms import ProductForm
+from django.shortcuts import render, redirect
 from productapp.models import Product
 
 # Create your views here.
@@ -10,24 +11,45 @@ def product_list(request):
 
 def product_detail(request,id):
     product = get_object_or_404(Product,id=id)
-    return render(request,'/home/sudhakarreddy/Djangoprojects/project5/productapp/templates/productapp/product_detail.html',{'product':product})
+    return render(request,'/home/sudhakarreddy/DjangoProjects/project5/productapp/templates/productapp/product_detail.html',{'product':product})
+
+
+def product_create(request):
+    form = ProductForm()
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("product_list")    
+    return render(request,'productapp/product_create.html',{'form':form})
+
+
+
+def product_update(request, id):
+    product = Product.objects.get(id=id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("product_list")
+    else:
+        form = ProductForm(instance=product)
+    return render(request, "productapp/product_update.html", {'product': product, 'form': form})
+
+
+
+# def product_delete(request,id):
+#     employee = Product.objects.get(id=id)
+#     employee.delete()
+#     return redirect(product_list)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def product_delete(request, id):
+    product = get_object_or_404(Product, id=id)  # Correct the argument to 'id'
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, '/home/sudhakarreddy/DjangoProjects/project5/productapp/templates/productapp/product_delete.html', {'product': product})
 
