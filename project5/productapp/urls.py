a6b736e96c378fb5710da4d8822e4ff6d6e8a4c3
sudhakarreddy31 <143116331+sudhakarreddy31@users.noreply.github.com>
2023-09-12 10:41:54 +0530

    Delete project5 directory

--- project5/productapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from productapp import views
-
-urlpatterns = [
-    path("product_list",views.product_list),
-    path("product_detail/<int:id>",views.product_detail,name='product_detail'),
-
-]
